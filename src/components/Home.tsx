eb9cabfaef4c86d1ab59477290d0859b23d46be2
Pedro Rojas <pedro.rojasm3@userena.cl>
2025-12-07 23:03:09 -0300

    Home update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,15 @@ import './Home.css'
 
 export function Home() {
   return (
-    <section id="home" className="content-section">
-      <h1>Welcome</h1>
-      <p>Software Developer & Designer</p>
+    <section id="home" className="home-section">
+      <div className="home-spacer"></div>
+      <h1 className="home-name">Pedro</h1>
+      <h1 className="home-name">Rojas</h1>
+      <h2 className="home-subtitle">Full stack developer</h2>
+      <p className="home-description">
+        Passionate about creating elegant solutions to complex problems. Experienced in modern web technologies and building scalable applications that make a difference.
+      </p>
     </section>
   )
 }
+
